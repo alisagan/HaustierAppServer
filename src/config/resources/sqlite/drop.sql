5fbec0b3d5b62902a3fe9eb11a0c520dca1490be
Alisa Ganter <gaal1019@h-ka.de>
2025-04-21 19:01:12 +0200

    SqlLite angepasst

--- src/config/resources/sqlite/drop.sql
+++ src/config/resources/sqlite/drop.sql
@@ -14,10 +14,10 @@
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 -- https://www.sqlite.org/lang_dropindex.html
-DROP INDEX IF EXISTS abbildung_buch_id_idx;
-DROP INDEX IF EXISTS buch_isbn_idx;
+DROP INDEX IF EXISTS foto_haustier_id_idx;
+DROP INDEX IF EXISTS haustier_name_idx;
 
 -- https://www.sqlite.org/lang_droptable.html
-DROP TABLE IF EXISTS abbildung;
-DROP TABLE IF EXISTS titel;
-DROP TABLE IF EXISTS buch;
+DROP TABLE IF EXISTS foto;
+DROP TABLE IF EXISTS beschreibung;
+DROP TABLE IF EXISTS haustier;
