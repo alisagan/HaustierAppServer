016aadd4e08534f3828f6630332e2d1eeb908640
Alisa Ganter <gaal1019@h-ka.de>
2025-04-21 14:13:14 +0200

    Dockerfile angepasst, Konfigurationsdateien für die DB angepasst, Mutation für graphQL angepasst, App.yml angepasst

--- src/config/dev/dev.module.ts
+++ src/config/dev/dev.module.ts
@@ -15,13 +15,13 @@
 
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Buch } from '../../buch/entity/buch.entity.js';
+import { Haustier } from '../../haustier/entity/haustier.entity.js';
 import { KeycloakModule } from '../../security/keycloak/keycloak.module.js';
 import { DbPopulateService } from './db-populate.service.js';
 import { DevController } from './dev.controller.js';
 
 @Module({
-    imports: [KeycloakModule, TypeOrmModule.forFeature([Buch])],
+    imports: [KeycloakModule, TypeOrmModule.forFeature([Haustier])],
     controllers: [DevController],
     providers: [DbPopulateService],
     exports: [DbPopulateService],
